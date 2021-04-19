8a285f68c2697a2d11b41f8b67bcce3554a90172
Dylan <dylandixon@hotmail.com>
2021-04-19 19:21:19 -0230

    Add functions for operators, add basic layout for html

---
+++ script.js
@@ -0,0 +1,62 @@
+function add(num1, ...num){
+    let numArgs = arguments.length;
+    let total = 0;
+
+    for(let i = 0; i < numArgs; i++){
+        total += arguments[i];
+    }
+
+    return total;
+}
+
+function subtract(num1, ...num){
+    let numArgs = arguments.length;
+    let total = num1;
+
+    for(let i = 0; i < numArgs; i++){
+        total -= arguments[i];
+    }
+
+    return total;
+}
+    
+function multiply(num1, ...num){
+    let numArgs = arguments.length;
+    let total = 1;
+
+    for(let i = 0; i < numArgs; i++){
+        total *= arguments[i];
+    }
+
+    return total;
+}
+
+function divide(num1, num2){
+    if(num2 === 0){
+        return 'Can\'t do that one, sorry.'
+    }
+
+    let total = num1 / num2;
+
+    return total;
+}
+
+function operate(operator, num1, num2){
+    switch(operator){
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+        default:
+            return 'ERROR';
+            break;
+    }
+}
