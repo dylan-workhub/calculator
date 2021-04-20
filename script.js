4f3b2d0dec25843fc247e0d0aec8f7c668ae7d0c
Dylan <dylandixon@hotmail.com>
2021-04-20 19:58:29 -0230

    Add break points for equals, dividing by 0

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const clearBtn = document.querySelector('#clear');
 let inputValue = 0;
 let currentNum = 0;
 let result = 0;
-let equalsPressed = false;
 let counter = 0;
+let decimalUsed = false;
 let operationArr = [];
 
 function add(num1, num2){
@@ -42,7 +42,7 @@ function operate(operator, num1, num2){
             return divide(num1, num2);
             break;
         default:
-            return 'ERROR';
+            return 'ERROR, please reset.';
             break;
     }
 }
@@ -52,13 +52,25 @@ function displayNumbers(){
         inputValue = this.innerText;
     }
     else{
-        inputValue += this.innerText;
+        if(this.innerText === '.'){
+            if(decimalUsed){
+                output.textContent = inputValue;
+            }
+            else{
+                decimalUsed = true;
+                inputValue += this.innerText;
+            }
+        }
+        else{
+            inputValue += this.innerText;
+        }
     }
     output.textContent = inputValue;
 }
 
 function operatorPressed(){
     counter++;
+    decimalUsed = false;
     inputValue = 0;
     operationArr[0] = operationArr[1];
     operationArr[1] = this.innerText;
@@ -68,12 +80,27 @@ function operatorPressed(){
     else{
         currentNum = output.textContent;
         result = operate(operationArr[0], result, currentNum);
-        output.textContent = result;
-        if(operationArr[1] === '='){
+        if(!(isFinite(result))){
+            output.textContent = 'Please do not divide by zero.';
             inputValue = 0;
             result = 0;
             operationArr = [];
             counter = 0;
+            decimalUsed = false;
+        }
+        else{
+            output.textContent = result;
+            console.table(operationArr);
+            if(operationArr[1] === '='){
+                inputValue = 0;
+                result = 0;
+                operationArr = [];
+                counter = 0;
+                decimalUsed = false;
+                if(operationArr[0] === '='){
+                    clearSlate();
+                }
+            }
         }
     }
 }
@@ -84,6 +111,7 @@ function clearSlate(){
     operationArr = [];
     output.textContent = 0;
     counter = 0;
+    decimalUsed = false;
 }
 
 buttons.forEach(item =>{
